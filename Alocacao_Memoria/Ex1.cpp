5a7731a5b879deb72d0a9aabfec177993b9f684f
Pedro H K Goncalves <108807271+PedroKons@users.noreply.github.com>
2022-10-04 09:02:37 -0300

    Create Ex1.cpp

---
+++ Alocacao_Memoria/Ex1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+Utilizando alocação dinâmica, declare um vetor de N inteiros (onde N é um valor fornecido
+pelo usuário), leia um valor para cada posição e no final mostre a média, o menor e o maior
+valor contidos no vetor */
+
+void preen(int vet[],int TAM){
+    for(int i=0;i<TAM;i++){
+        cout<<"Digite os valores: ";
+        cin>>vet[i];
+
+        
+    }
+    cout<<endl;
+}
+
+float media(int vet[],int TAM){
+    int soma = vet[0];
+    for(int i=0;i<TAM;i++){
+        soma=soma+vet[i];
+        soma=soma/TAM;
+    }
+    return (float) soma;
+}
+
+int maior (int vet[],int TAM){
+    int maior = vet[0];
+    for(int i=0;i<TAM;i++){
+        if (vet[i]>maior){
+            maior=vet[i];
+        }
+    }
+    return maior;
+}
+
+int menor (int vet[],int TAM){
+    int menor = vet[0];
+    for(int i=0;i<TAM;i++){
+        if(vet[i]<menor){
+            menor=vet[i];
+        }
+    }
+    return menor;
+}
+
+
+
+int main(){
+
+int maior1,menor1,TAM;
+float media1;
+
+cout<<"Digite o Tamanho! : ";
+cin>>TAM;
+
+int *vet;
+vet= new int[TAM];
+
+preen(vet,TAM);
+
+media1 = media(vet,TAM);
+maior1 = maior(vet,TAM);
+menor1 = menor(vet,TAM);
+
+cout<<"Media: "<<media1;
+cout<<endl;
+cout<<"Maior: "<<maior1;
+cout<<endl;
+cout<<"Menor: "<<menor1;
+
+delete []vet;
+
+
+}
